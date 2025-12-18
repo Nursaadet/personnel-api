33012712d227c9ddaed24f4ab7dc0db88fd15a38
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-18 21:41:37 +0300

    chore(swagger): add swagger-autogen options template

--- swaggerAutogen.js
+++ swaggerAutogen.js
@@ -4,9 +4,24 @@
 ------------------------------------------------------- */
 // Swagger Autogen
 // https://swagger-autogen.github.io/docs/
+// $ npm i swagger-autogen # JSON creator
+// $ npm i swagger-ui-express
+// $ npm i redoc-express
 /* ------------------------------------------------------- */
 
 require('dotenv').config()
 const HOST = process.env?.HOST || 'http://127.0.0.1'
 const PORT = process.env?.PORT || 8000
+
+/* ------------------------------------------------------- *
+const options = {
+	openapi:          <string>,     // Enable/Disable OpenAPI.                        By default is null
+	language:         <string>,     // Change response language.                      By default is 'en-US'
+	disableLogs:      <boolean>,    // Enable/Disable logs.                           By default is false
+	autoHeaders:      <boolean>,    // Enable/Disable automatic headers recognition.  By default is true
+	autoQuery:        <boolean>,    // Enable/Disable automatic query recognition.    By default is true
+	autoBody:         <boolean>,    // Enable/Disable automatic body recognition.     By default is true
+	writeOutputFile:  <boolean>     // Enable/Disable writing the output file.        By default is true
+};
 /* ------------------------------------------------------- */
+const swaggerAutogen = require('')
