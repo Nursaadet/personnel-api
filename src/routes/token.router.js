bdf488e1aa3b8debafd8857a1f2385f34b30cbe5
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-18 14:39:25 +0300

    feat(token): add CRUD routes for token controller

--- src/routes/token.router.js
+++ src/routes/token.router.js
@@ -3,11 +3,20 @@
     EXPRESS - Personnel API
 ------------------------------------------------------- */
 
-const router = require('express').Router()
+const router = require("express").Router();
+/* ------------------------------------------------------- */
+
+const token = require("../controllers/token.controller");
 
 /* ------------------------------------------------------- */
 
+router.route("/").get(token.list).post(token.create);
 
+router
+  .route("/:id")
+  .get(token.read)
+  .put(token.update)
+  .patch(token.update)
+  .delete(token.delete);
 
-/* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
