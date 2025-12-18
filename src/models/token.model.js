65af24fa2c500a67779621da7153bc513250ec2c
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-18 14:28:24 +0300

    feat(token): add token mongoose model

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -5,6 +5,30 @@
 const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
 
+// Token Model:
 
+const TokenSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Personnel',
+        required: true,
+        index: true
+    },
+
+    token: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
+    }
+
+}, {
+
+    collection: 'tokens',
+    timestamps: true
+
+})
 
 /* ------------------------------------------------------- */
