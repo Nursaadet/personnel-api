9ffac0fb3fe40586931129134cc8d59442abddb1
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-18 21:38:41 +0300

    chore: add env-based host and port config for swagger autogen

--- swaggerAutogen.js
+++ swaggerAutogen.js
@@ -2,5 +2,11 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+// Swagger Autogen
+// https://swagger-autogen.github.io/docs/
+/* ------------------------------------------------------- */
 
+require('dotenv').config()
+const HOST = process.env?.HOST || 'http://127.0.0.1'
+const PORT = process.env?.PORT || 8000
 /* ------------------------------------------------------- */
