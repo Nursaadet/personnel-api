8e91f4106d5fb4d321e3f53842c7bde2ba1ae56d
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-18 17:38:37 +0300

    feat(permissions): add role-based access control middleware

---
+++ src/middlewares/permissions.js
@@ -0,0 +1,42 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESS - Personnel API
+------------------------------------------------------- */
+// Permission Control Middleware:
+
+module.exports = {
+  isLogin: (req, res, next) => {
+    if (req.user && req.user.isActive) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login.");
+    }
+  },
+
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isActive && req.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Admin.");
+    }
+  },
+
+  isAdminOrLead: (req, res, next) => {
+    if (
+      req.user &&
+      req.user.isActive &&
+      (req.user.isAdmin || req.user.isLead)
+    ) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error(
+        "NoPermission: You must login and to be Admin or Department Lead."
+      );
+    }
+  },
+};
+
+/* ------------------------------------------------------- */
