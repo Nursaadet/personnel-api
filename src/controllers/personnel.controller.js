40fe994aac9610d564d8edcabc7e0a8d773b43ec
Nursaadet <nursaadetozdemir@gmail.com>
2025-12-19 23:16:49 +0300

    docs(personnels): improve Swagger documentation for personnels endpoints

--- src/controllers/personnel.controller.js
+++ src/controllers/personnel.controller.js
@@ -6,6 +6,19 @@ const Personnel = require("../models/personnel.model");
 
 module.exports = {
   list: async (req, res) => {
+    /*
+        #swagger.tags = ["Personnels"]
+        #swagger.summary = "List Personnels"
+        #swagger.description = `
+            You can send query with endpoint for search[], sort[], page and limit.
+            <ul> Examples:
+                <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                <li>URL/?<b>page=2&limit=1</b></li>
+            </ul>
+        `
+    */
     const data = await res.getModelList(Personnel);
 
     res.status(200).send({
@@ -16,6 +29,17 @@ module.exports = {
   },
 
   create: async (req, res) => {
+    /*
+        #swagger.tags = ["Personnels"]
+        #swagger.summary = "Create Personnel"
+        #swagger.parameters['body'] = {
+            in: 'body',
+            required: true,
+            schema: {
+                $ref: '#/definitions/Personnel'
+            }
+        }
+    */
     // const isFirstAccount = (await Personnel.countDocuments()) === 0;
     // req.body.isAdmin = isFirstAccount ? true : false;
 
@@ -37,6 +61,10 @@ module.exports = {
   },
 
   read: async (req, res) => {
+    /*
+        #swagger.tags = ["Personnels"]
+        #swagger.summary = "Get Single Personnel"
+    */
     const data = await Personnel.findOne({ _id: req.params.id });
 
     res.status(200).send({
@@ -46,6 +74,17 @@ module.exports = {
   },
 
   update: async (req, res) => {
+    /*
+        #swagger.tags = ["Personnels"]
+        #swagger.summary = "Update Personnel"
+        #swagger.parameters['body'] = {
+            in: 'body',
+            required: true,
+            schema: {
+                $ref: '#/definitions/Personnel'
+            }
+        }
+    */
     // isLead Control:
     const isLead = req.body?.isLead || false;
     if (isLead) {
@@ -70,6 +109,10 @@ module.exports = {
     });
   },
   delete: async (req, res) => {
+    /*
+        #swagger.tags = ["Personnels"]
+        #swagger.summary = "Delete Personnel"
+    */
     const data = await Personnel.deleteOne({ _id: req.params.id });
 
     res.status(data.deletedCount ? 204 : 404).send({
@@ -85,7 +128,7 @@ module.exports = {
 
     if (username && password) {
       const user = await Personnel.findOne({ username, password });
-      if (user  && user.isActive) {
+      if (user && user.isActive) {
         // Set Session:
         req.session = {
           id: user._id,
